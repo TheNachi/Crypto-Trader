05c412960f4268cb144207feacf28eb7425f0fee
Munachimso Ugorji <ugorjimunachimso@gmail.com>
2024-02-22 18:49:21 +0100

    Initial Commit

---
+++ CryptoTrader/CryptoTraderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTraderApp.swift
+//  CryptoTrader
+//
+//  Created by Munachimso Ugorji on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTraderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
